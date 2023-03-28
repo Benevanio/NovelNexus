69f7c347061aa4171348f5ee02b7ccf60d36614e
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-28 19:14:19 -0300

    Criando condicionais para meu estoque

--- src/CalculadoreDeEstoque.java
+++ src/CalculadoreDeEstoque.java
@@ -1,18 +1,24 @@
 public class CalculadoreDeEstoque {
     public static void main(String[] args) {
-        double LivroToradora = 89.90;
-        double LivroReZero = 80;
+        double LivroToradora = 90;
+        double LivroReZero = 100;
 
-        int quantidadeLivroToradora = 2;
+        int quantidadeLivroToradora = 20;
         int quantidadeLivroReZero = 3;
 
         double valorTotalLivroToradora = LivroToradora * quantidadeLivroToradora;
         double valorTotalLivroReZero = LivroReZero * quantidadeLivroReZero;
 
         double valorTotalEstoque = valorTotalLivroToradora + valorTotalLivroReZero;
-
+        
         System.out.println("O valor total do estoque é: " + valorTotalEstoque);
-
+        if (valorTotalEstoque < 200) {
+            System.out.println("Seu estoque está muito baixo");
+        } else if (valorTotalEstoque >= 200 && valorTotalEstoque <= 500) {
+            System.out.println("Seu estoque está bom");
+        } else if (valorTotalEstoque > 500) {
+            System.out.println("Seu estoque está muito alto, considere vender alguns produtos");
+        }
     }
 
 }
