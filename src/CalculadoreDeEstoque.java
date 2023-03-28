4e85f51f15267e17f04a258dc56c13b211d8a7b7
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-28 19:39:37 -0300

    Criando o meu primeiro loop neste sistema

--- src/CalculadoreDeEstoque.java
+++ src/CalculadoreDeEstoque.java
@@ -2,6 +2,10 @@ public class CalculadoreDeEstoque {
     public static void main(String[] args) {
         double LivroToradora = 90;
         double LivroReZero = 100;
+        double soma =0;
+        int contador = 0;
+
+
 
         int quantidadeLivroToradora = 20;
         int quantidadeLivroReZero = 3;
@@ -11,6 +15,10 @@ public class CalculadoreDeEstoque {
 
         double valorTotalEstoque = valorTotalLivroToradora + valorTotalLivroReZero;
         
+        while (contador < 10){
+            soma = soma + LivroToradora;
+            contador = contador +5;
+        }
         System.out.println("O valor total do estoque é: " + valorTotalEstoque);
         if (valorTotalEstoque < 150){
             System.out.println("Por favor, aumente o valor do estoque");
