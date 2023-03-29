0f15e05908ca958dcc5a6304d1ac62415735e47d
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-29 08:34:17 -0300

    Criando o objeto livro

--- src/CalculadoreDeEstoque.java
+++ src/CalculadoreDeEstoque.java
@@ -1,6 +1,14 @@
 public class CalculadoreDeEstoque {
     public static void main(String[] args) {
-       
+        Livro livro = new Livro();
+        livro.nome = "Toradora!";
+        livro.descricao = "Um romance  entre um garoto e uma garota";
+        livro.valor = 59.90;
+        livro.isbn = "978-85-7522-480-7";
+        livro.categoria = "Romance";
+        livro.autor = "Yuyuko Takemiya";
+        livro.editora = "JBC";
+        System.out.println("O livro " + livro.nome + " foi adicionado ao estoque");
     }
 
 }
