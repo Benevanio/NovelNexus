6a28a5c98226f34f58bd4dd97e30ea7187211235
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-29 09:13:57 -0300

    adicinando um metodo para mostra os detalhes do nosso livro

--- src/Livro.java
+++ src/Livro.java
@@ -6,4 +6,14 @@ public class Livro {
     String categoria;
     String autor;
     String editora;
+    void mostrarDetalhes() {
+        System.out.println("Mostrando os detalhes do livro");
+        System.out.println("Nome: " + nome);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Valor: " + valor);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Autor: " + autor);
+        System.out.println("Editora: " + editora);
+    }
 }
