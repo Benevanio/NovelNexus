7831d266b6515bc24d5b76cf3a8ce5ad184ae9e7
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-29 08:04:44 -0300

    Sistema web

--- src/CalculadoreDeEstoque.java
+++ src/CalculadoreDeEstoque.java
@@ -2,15 +2,15 @@ public class CalculadoreDeEstoque {
     public static void main(String[] args) {
         double LivroToradora = 80;
         double LivroReZero = 100;
-       
-        //usar o for para aplicar o desconto nos livros
-        for (int i = 0; i < 1; i++){
+
+        // usar o for para aplicar o desconto nos livros
+        for (int i = 0; i < 1; i++) {
             LivroToradora = LivroToradora - (LivroToradora * 0.1);
             System.out.println("O valor do livro Toradora com desconto é: " + LivroToradora);
             LivroReZero = LivroReZero - (LivroReZero * 0.1);
             System.out.println("O valor do livro ReZero com desconto é: " + LivroReZero);
         }
-        
+
         int quantidadeLivroToradora = 20;
         int quantidadeLivroReZero = 3;
 
@@ -18,17 +18,15 @@ public class CalculadoreDeEstoque {
         double valorTotalLivroReZero = LivroReZero * quantidadeLivroReZero;
 
         double valorTotalEstoque = valorTotalLivroToradora + valorTotalLivroReZero;
-        
-        
+
         System.out.println("O valor total do estoque é: " + valorTotalEstoque);
-        if (valorTotalEstoque < 150){
+        if (valorTotalEstoque < 150) {
             System.out.println("Por favor, aumente o valor do estoque");
-        } else if (valorTotalEstoque >= 2000){
+        } else if (valorTotalEstoque >= 2000) {
             System.out.println("O seu estoque se encontra em niveis aceitaveis");
         } else {
             System.out.println("O seu estoque se encontra em niveis criticos");
         }
     }
-   
 
 }
