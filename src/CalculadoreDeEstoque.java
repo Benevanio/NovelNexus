e36e7622cb6828ca329d2c4bb4dc17c266e36e2b
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-29 08:15:12 -0300

    Criando a classe e seus atributos

--- src/CalculadoreDeEstoque.java
+++ src/CalculadoreDeEstoque.java
@@ -1,30 +1,6 @@
 public class CalculadoreDeEstoque {
     public static void main(String[] args) {
-        double LivroToradora = 80;
-        double LivroReZero = 100;
-        for (int i = 0; i < 1; i++) {
-            LivroToradora = LivroToradora - (LivroToradora * 0.1);
-            System.out.println("O valor do livro Toradora com desconto é: " + LivroToradora);
-            LivroReZero = LivroReZero - (LivroReZero * 0.1);
-            System.out.println("O valor do livro ReZero com desconto é: " + LivroReZero);
-        }
-
-        int quantidadeLivroToradora = 20;
-        int quantidadeLivroReZero = 3;
-
-        double valorTotalLivroToradora = LivroToradora * quantidadeLivroToradora;
-        double valorTotalLivroReZero = LivroReZero * quantidadeLivroReZero;
-
-        double valorTotalEstoque = valorTotalLivroToradora + valorTotalLivroReZero;
-
-        System.out.println("O valor total do estoque é: " + valorTotalEstoque);
-        if (valorTotalEstoque < 150) {
-            System.out.println("Por favor, aumente o valor do estoque");
-        } else if (valorTotalEstoque >= 2000) {
-            System.out.println("O seu estoque se encontra em niveis aceitaveis");
-        } else {
-            System.out.println("O seu estoque se encontra em niveis criticos");
-        }
+       
     }
 
 }
---
+++ src/Livro.java
@@ -0,0 +1,9 @@
+public class Livro {
+    String nome;
+    String descricao;
+    double valor;
+    String isbn;
+    String categoria;
+    String autor;
+    String editora;
+}
