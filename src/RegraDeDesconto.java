ed02c0c62c41b89954b603cb544d8d7f89bc9e95
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-29 18:43:53 -0300

    Ja temos as seguintes regras: Cadastro de livros com o metodo PSVM e regra de desconto ambos com o psvm, a regra de desconto vai pegar o valor do livro e aplicar um desconto no valor de 0 ate 30% no mesmo

--- src/RegraDeDesconto.java
+++ src/RegraDeDesconto.java
@@ -1,18 +1,27 @@
-
 import java.util.Scanner;
 public class RegraDeDesconto {
     public static void main(String[] args) {
-
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("Digite o valor do livro: ");
-        double valor = leitor.nextDouble();
-        System.out.println("Digite a porcentagem de desconto: ");
-        double porcentagem = leitor.nextDouble();
         Livro livro = new Livro();
-        livro.setValor(valor);
-        livro.aplicardDesconto(porcentagem);
-        System.out.println("O valor do livro com desconto é: " + livro.getValor());
-        leitor.close();
+        livro.nome = "No game No life";
+        livro.descricao = "Um livro sobre um jogo";
+        livro.isbn = "978-85-66250-46-6";
+        livro.categoria = "Aventura";
+        livro.editora = "JBC";
+        livro.setValor(59.90);
+        Autor autor = new Autor();
+        autor.nome = "Yuu Kamiya";
+        autor.email = "YuuKa@gmail.com";
+        autor.cpf = "123.456.789-10";
 
+
+       //aplicar o desconto no livro
+        Scanner valorDesconto = new Scanner(System.in);
+        System.out.println("Digite o valor do desconto: ");
+
+        double desconto = valorDesconto.nextDouble();
+        livro.aplicardDesconto(desconto);
+        livro.mostrarDetalhes();
+        valorDesconto.close();
+    
     }
 }
