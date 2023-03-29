7003fa03227d4b082544f9814c005c45fdae186c
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-29 08:53:11 -0300

    Adicionando um novo livro a nossa editora

--- src/CalculadoreDeEstoque.java
+++ src/CalculadoreDeEstoque.java
@@ -8,6 +8,32 @@ public class CalculadoreDeEstoque {
         livro.categoria = "Romance";
         livro.autor = "Yuyuko Takemiya";
         livro.editora = "JBC";
+        System.out.println("Nome: " + livro.nome);
+        System.out.println("Descrição: " + livro.descricao);
+        System.out.println("Valor: " + livro.valor);
+        System.out.println("ISBN: " + livro.isbn);
+        System.out.println("Categoria: " + livro.categoria);
+        System.out.println("Autor: " + livro.autor);
+        System.out.println("Editora: " + livro.editora);
+
+        System.out.println("O livro " + livro.nome + " foi adicionado ao estoque");
+
+        livro = new Livro();
+        livro.nome = "O Senhor dos Anéis";
+        livro.descricao = "O Senhor dos Anéis é uma trilogia de livros de fantasia escrita por J. R. R. Tolkien. A trilogia é composta por O Hobbit, O Senhor dos Anéis e O Silmarillion. O Senhor dos Anéis é uma sequência direta de O Hobbit, e conta a história de Frodo Bolseiro, um hobbit que recebe de seu tio Bilbo um anel mágico, o Um Anel, que pertenceu ao poderoso Sauron, o Senhor do Escuro. Sauron deseja recuperar o anel para recuperar o seu poder e dominar a Terra-média. Frodo, com a ajuda de um grupo de amigos, deve destruir o anel antes que Sauron o recupere.";
+        livro.valor = 59.90;
+        livro.isbn = "978-85-7522-480-7";
+        livro.categoria = "Fantasia";
+        livro.autor = "J. R. R. Tolkien";
+        livro.editora = "JBC";
+        System.out.println("Nome: " + livro.nome);
+        System.out.println("Descrição: " + livro.descricao);
+        System.out.println("Valor: " + livro.valor);
+        System.out.println("ISBN: " + livro.isbn);
+        System.out.println("Categoria: " + livro.categoria);
+        System.out.println("Autor: " + livro.autor);
+        System.out.println("Editora: " + livro.editora);
+
         System.out.println("O livro " + livro.nome + " foi adicionado ao estoque");
     }
 
