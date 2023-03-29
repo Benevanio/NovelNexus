43d015ab8f84bc33683d33bf02a2819ba152db16
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-29 09:34:09 -0300

    Criando uma nova light novel no sistema.

--- src/CadastroDeLivro.java
+++ src/CadastroDeLivro.java
@@ -9,6 +9,17 @@ public class CadastroDeLivro {
         livro.autor = "Yuyuko Takemiya";
         livro.editora = "JBC";
         livro.mostrarDetalhes();
-        }
+
+        Livro outroLivro = new Livro();
+        outroLivro.nome = "No Game No Life";
+        outroLivro.descricao = "No Game No Life é uma série de light novel japonesa escrita por Yuu Kamiya e ilustrada por Mashiro Hiiragi. A série possui 10 volumes, os quais foram lançados entre 10 de março de 2012 e 10 de março de 2014. A história segue a vida de Sora e Shiro, dois irmãos que são considerados os melhores jogadores de jogos de tabuleiro do mundo. Eles são convidados por Deus para participar de um jogo que decidirá o futuro da humanidade.";
+        outroLivro.valor = 29.90;
+        outroLivro.isbn = "978-85-7522-480-7";
+        outroLivro.categoria = "Aventura";
+        outroLivro.autor = "Yuu Kamiya";
+        outroLivro.editora = "JBC";
+        outroLivro.mostrarDetalhes();
+        
+    }
 
 }
