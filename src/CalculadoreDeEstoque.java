4caa2ae1f1f81bdd8effc16fba1246b5f951e1c4
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-29 08:42:54 -0300

    Adicionando uma descrição para o livro

--- src/CalculadoreDeEstoque.java
+++ src/CalculadoreDeEstoque.java
@@ -2,7 +2,7 @@ public class CalculadoreDeEstoque {
     public static void main(String[] args) {
         Livro livro = new Livro();
         livro.nome = "Toradora!";
-        livro.descricao = "Um romance  entre um garoto e uma garota";
+        livro.descricao = "Toradora! é uma série de light novel escrita por Yuyuko Takemiya e ilustrada por Yasu. A série possui dez volumes, os quais foram lançados entre 10 de março de 2006 e 10 de março de 2009. A história segue a vida de Ryuuji Takasu, um garoto mal-encarado, mas de bom coração, que começa o seu segundo ano do colégio conhecendo a Taiga Aisaka, uma garota baixinha, porém de temperamento feroz, e que é temida por todos pela alcunha de “Tigresa de Bolso";
         livro.valor = 59.90;
         livro.isbn = "978-85-7522-480-7";
         livro.categoria = "Romance";
