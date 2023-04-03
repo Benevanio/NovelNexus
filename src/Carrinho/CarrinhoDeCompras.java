8b1de097923b8864c8ec2aff6bc1757f8c2a0187
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-03 18:03:21 -0300

    Modificando o operador no carrinho de compras

--- src/Carrinho/CarrinhoDeCompras.java
+++ src/Carrinho/CarrinhoDeCompras.java
@@ -2,11 +2,11 @@ package Carrinho;
 
 import produtos.Produto;
 import java.lang.ArrayIndexOutOfBoundsException;
+
 public class CarrinhoDeCompras {
 
     private double total;
     private Produto[] produtos = new Produto[10];
-
 
     public void adiciona(Produto produto) {
         for (int i = 0; i <= produtos.length; i++) {
@@ -15,18 +15,14 @@ public class CarrinhoDeCompras {
                     produtos[i] = produto;
                     total += produto.getValor();
                     break;
-                    
-            }
+
+                }
             } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("deu exception no indice: "+ i);
+                System.out.println("deu exception no indice: " + i);
             }
-            }
-            System.out.println("Fui executado!");
+        }
+        System.out.println("Fui executado!");
     }
-
-    // total = new java.math.BigDecimal(total + produto.getValor()).setScale(1,
-    // java.math.RoundingMode.HALF_EVEN).doubleValue();
-
     public double getTotal() {
         return total;
     }
