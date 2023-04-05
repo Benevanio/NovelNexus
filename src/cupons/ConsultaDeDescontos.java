b89971b68e6bbf6b157c5523192e559d38d81123
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-05 11:32:27 -0300

    Finalizando o sistema

--- src/cupons/ConsultaDeDescontos.java
+++ src/cupons/ConsultaDeDescontos.java
@@ -2,14 +2,14 @@ package cupons;
 
 public class ConsultaDeDescontos {
     public static void main(String[] args) {
-
         GerenciadorDeCupons gerenciador = new GerenciadorDeCupons();
+
         Double desconto = gerenciador.getDesconto("CUPOM10");
-        if(desconto != null) {
+        if (desconto != null) {
             System.out.println("Cupom válido");
             System.out.println("Desconto: " + desconto + "%");
         } else {
             System.out.println("Cupom inválido");
-        }    
+        }
     }
 }
